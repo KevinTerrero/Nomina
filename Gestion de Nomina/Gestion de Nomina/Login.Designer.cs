3f21fcfe22591826e7ceb81b8a6c0f308a1ac627
KevinTerrero <94633933+KevinTerrero@users.noreply.github.com>
2023-04-12 19:22:41 -0400

    Add files via upload

---
+++ Gestion de Nomina/Gestion de Nomina/Login.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Gestion_de_Nomina
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.PasswordTxtBox = new System.Windows.Forms.TextBox();
+            this.UserTxtBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaLb = new System.Windows.Forms.Label();
+            this.UsuarioLb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(67, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sistema de gestión de nómina";
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitBtn.Location = new System.Drawing.Point(197, 280);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(118, 34);
+            this.SubmitBtn.TabIndex = 10;
+            this.SubmitBtn.Text = "Login";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // PasswordTxtBox
+            // 
+            this.PasswordTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PasswordTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTxtBox.Location = new System.Drawing.Point(153, 228);
+            this.PasswordTxtBox.Name = "PasswordTxtBox";
+            this.PasswordTxtBox.PasswordChar = '*';
+            this.PasswordTxtBox.Size = new System.Drawing.Size(241, 26);
+            this.PasswordTxtBox.TabIndex = 9;
+            this.PasswordTxtBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PasswordTxtBox_MouseDown);
+            // 
+            // UserTxtBox
+            // 
+            this.UserTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserTxtBox.Location = new System.Drawing.Point(153, 172);
+            this.UserTxtBox.Name = "UserTxtBox";
+            this.UserTxtBox.Size = new System.Drawing.Size(241, 26);
+            this.UserTxtBox.TabIndex = 8;
+            this.UserTxtBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UserTxtBox_MouseDown);
+            // 
+            // ContraseñaLb
+            // 
+            this.ContraseñaLb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ContraseñaLb.AutoSize = true;
+            this.ContraseñaLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContraseñaLb.Location = new System.Drawing.Point(16, 228);
+            this.ContraseñaLb.Name = "ContraseñaLb";
+            this.ContraseñaLb.Size = new System.Drawing.Size(131, 25);
+            this.ContraseñaLb.TabIndex = 7;
+            this.ContraseñaLb.Text = "Contraseña:";
+            // 
+            // UsuarioLb
+            // 
+            this.UsuarioLb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UsuarioLb.AutoSize = true;
+            this.UsuarioLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuarioLb.Location = new System.Drawing.Point(54, 172);
+            this.UsuarioLb.Name = "UsuarioLb";
+            this.UsuarioLb.Size = new System.Drawing.Size(93, 25);
+            this.UsuarioLb.TabIndex = 6;
+            this.UsuarioLb.Text = "Usuario:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 452);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.PasswordTxtBox);
+            this.Controls.Add(this.UserTxtBox);
+            this.Controls.Add(this.ContraseñaLb);
+            this.Controls.Add(this.UsuarioLb);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.TextBox PasswordTxtBox;
+        private System.Windows.Forms.TextBox UserTxtBox;
+        private System.Windows.Forms.Label ContraseñaLb;
+        private System.Windows.Forms.Label UsuarioLb;
+    }
+}
+
---
+++ Gestion de Nomina/Gestion de Nomina/Login.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_de_Nomina
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Clear(TextBox t)
+        {
+            t.Clear();
+        }
+
+        private void MostrarError(TextBox t)
+        {
+            t.Text = "Tiene que llenar este campo";
+            t.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+        private void UserValue()
+        {
+            string username = UserTxtBox.Text;
+            string password = PasswordTxtBox.Text;
+            if (UserTxtBox.Text == "")
+            {
+                MostrarError(UserTxtBox);
+            }
+
+            if (PasswordTxtBox.Text == "")
+            {
+                PasswordTxtBox.PasswordChar= '\0';
+                MostrarError(PasswordTxtBox);
+            }
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+
+            UserValue();
+        }
+
+        private void UserTxtBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            Clear(UserTxtBox);
+            UserTxtBox.ForeColor = Color.Black;
+        }
+
+        private void PasswordTxtBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            Clear(PasswordTxtBox);
+            PasswordTxtBox.PasswordChar = '*';
+            PasswordTxtBox.ForeColor = Color.Black;
+        }
+    }
+}
