7407b2b399da23eacab9db213b4297c63288cd89
Antonio Dilone <antoniodilone26@gmail.com>
2023-04-22 08:50:25 -0400

    Pruebita

--- Gestion de Nomina/Gestion de Nomina/Reporte.cs
+++ Gestion de Nomina/Gestion de Nomina/Reporte.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Gestion_de_Nomina
 {
@@ -19,7 +21,12 @@ namespace Gestion_de_Nomina
 
         private void pdfViewer1_Load(object sender, EventArgs e)
         {
-
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=LAPTOP-QM5FFGMO\\SQLEXPRESS;Initial Catalog=Agenda;Integrated Security=True;";
+            
+            conn.Open(); 
+            
+            conn.Close();
         }
     }
 }
